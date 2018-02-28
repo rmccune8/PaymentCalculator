17627781953de28a82c46681351f4297056b7e06
Ryan CTR McCune <Ryan CTR McCune@AVSL001214187>
2018-02-28 08:37:51 -0600

    Add project files.

---
+++ LoanPaymentCalculator/Business/ILoanPaymentCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanPaymentCalculator.Business
+{
+    public interface ILoanPaymentCalculator
+    {
+        IPaymentResult Calculate(decimal amount, decimal interest, decimal downPayment, decimal term);
+
+            // input amount, interest (percentage or digit), downpayment, term, last line blank
+            // output json of monthly pmt, tot interest, tot payment
+    }
+}
---
+++ LoanPaymentCalculator/Business/PaymentResult.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanPaymentCalculator.Business
+{
+    public class PaymentResult : IPaymentResult
+    {
+        private readonly decimal m_monthlyPayment = 0m;
+        private readonly decimal m_totalInterest = 0m;
+        private readonly decimal m_totalPayment = 0m;
+
+        public PaymentResult(decimal monthlyPayment, decimal totalInterest,
+            decimal totalPayment)
+        {
+            m_monthlyPayment = monthlyPayment;
+            m_totalInterest = totalInterest;
+            m_totalPayment = totalPayment;
+        }
+
+        public decimal MonthlyPayment
+        {
+            get
+            {
+                return m_monthlyPayment;
+            }
+        }
+
+        public decimal TotalInterest
+        {
+            get
+            {
+                return m_totalInterest;
+            }
+        }
+
+        public decimal TotalPayment
+        {
+            get
+            {
+                return m_totalPayment;
+            }
+        }
+    }
+}
